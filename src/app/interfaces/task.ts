4619a592a623211b2fc4969852200d6abcdf5c45
drugpamir <toll_aa@mail.ru>
2025-09-22 23:50:40 +0300

    Hw-2 component styles

--- src/app/interfaces/task.ts
+++ src/app/interfaces/task.ts
@@ -1,12 +1,12 @@
 export interface Task {
   id: number;
-  name: string;
+  text: string;
 }
 
 export function getTasks(): Task[] {
   return [
-    { id: 1, name: "Visit a market" },
-    { id: 2, name: "Wash the dishes" },
-    { id: 3, name: "Choose a gift" },
+    { id: 1, text: "Visit a market" },
+    { id: 2, text: "Wash the dishes" },
+    { id: 3, text: "Choose a gift" },
   ]
 }
