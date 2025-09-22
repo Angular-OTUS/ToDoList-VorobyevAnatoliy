d553ad36429d7a7f00aa421fb4b0cf772a4744dd
drugpamir <toll_aa@mail.ru>
2025-09-23 00:49:35 +0300

    "Add task" button with disabled style

--- src/app/components/to-do-list/to-do-list.ts
+++ src/app/components/to-do-list/to-do-list.ts
@@ -1,15 +1,21 @@
 import {Component, OnInit} from '@angular/core';
 import {getTasks, Task} from '../../interfaces/task';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-to-do-list',
   templateUrl: './to-do-list.html',
   standalone: true,
+  imports: [
+    FormsModule
+  ],
   styleUrl: './to-do-list.css'
 })
 export class ToDoList implements OnInit {
 
-  tasks: Task[] | undefined
+  tasks: Task[] = []
+
+  newTaskText: string = ""
 
   ngOnInit(): void {
     this.tasks = getTasks()
