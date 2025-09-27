b57819938858eb1f386b703a7739963ef460cf35
drugpamir <toll_aa@mail.ru>
2025-09-28 00:50:45 +0300

    added tooltip to buttons

--- src/app/components/to-do-list-item/to-do-list-item.ts
+++ src/app/components/to-do-list-item/to-do-list-item.ts
@@ -1,11 +1,13 @@
 import {Component, input, output} from '@angular/core';
 import {Task} from '../../models/task';
 import {Button} from '../button/button';
+import {TooltipDirective} from '../../directives/tooltip';
 
 @Component({
   selector: 'app-to-do-list-item',
   imports: [
     Button,
+    TooltipDirective,
   ],
   templateUrl: './to-do-list-item.html',
   standalone: true,
