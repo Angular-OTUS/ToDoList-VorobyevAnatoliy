c930d20fee1ad203af154da57206186adebca59b
drugpamir <toll_aa@mail.ru>
2025-09-29 23:30:17 +0300

    app-button signals for action type class are replaced by button--{{action()}

--- src/app/components/button/button.ts
+++ src/app/components/button/button.ts
@@ -1,4 +1,4 @@
-import {Component, computed, input} from '@angular/core';
+import {Component, input} from '@angular/core';
 import {NgClass} from '@angular/common';
 import {ActionType} from './action-types';
 
@@ -18,10 +18,4 @@ export class Button {
   readonly disabled = input( false);
 
   readonly action = input<ActionType>('none')
-
-  readonly isAdd = computed(() => this.action() === 'add')
-
-  readonly isDelete = computed(() => this.action() === 'delete')
-
-  readonly isSave = computed(() => this.action() === 'save')
 }
