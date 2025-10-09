ac29e2bfe01940bbe6232d4e0f2dd256a1fa0fde
drugpamir <toll_aa@mail.ru>
2025-10-09 23:02:53 +0300

    task-storage service uses takeUntilDestroyed methods

--- src/app/services/task-storage.service.ts
+++ src/app/services/task-storage.service.ts
@@ -1,7 +1,8 @@
-import {inject, Injectable} from '@angular/core';
+import {DestroyRef, inject, Injectable} from '@angular/core';
 import {Task, TaskData} from '../models/task';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 
 @Injectable({
   providedIn: 'root',
@@ -12,19 +13,29 @@ export class TaskStorageService {
 
   private http = inject(HttpClient)
 
+  private destroy$ = inject(DestroyRef)
+
   public getTasks(): Observable<Task[]> {
-    return this.http.get<Task[]>(this.URL_TASKS)
+    return this.http.get<Task[]>(this.URL_TASKS).pipe(
+      takeUntilDestroyed(this.destroy$),
+    )
   }
 
   public addTask(task: TaskData): Observable<Task> {
-    return this.http.post<Task>(this.URL_TASKS, task)
+    return this.http.post<Task>(this.URL_TASKS, task).pipe(
+      takeUntilDestroyed(this.destroy$),
+    )
   }
 
   public deleteTask(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.URL_TASKS}/${id}`)
+    return this.http.delete<void>(`${this.URL_TASKS}/${id}`).pipe(
+      takeUntilDestroyed(this.destroy$),
+    )
   }
 
   public updateTask(taskId: number, updatedData: Partial<TaskData>): Observable<Task> {
-    return this.http.patch<Task>(`${this.URL_TASKS}/${taskId}`, updatedData)
+    return this.http.patch<Task>(`${this.URL_TASKS}/${taskId}`, updatedData).pipe(
+      takeUntilDestroyed(this.destroy$),
+    )
   }
 }
