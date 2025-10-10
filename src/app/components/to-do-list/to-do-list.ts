ef16e15cc308c5b6c13aa4fd71cd673b98c24878
drugpamir <toll_aa@mail.ru>
2025-10-10 19:35:34 +0300

    unsubscribe from observable and moved side effects from subscribe() to pipe(). Gratitude to tutor (hw-6)

--- src/app/components/to-do-list/to-do-list.ts
+++ src/app/components/to-do-list/to-do-list.ts
@@ -9,6 +9,7 @@ import {Toasts} from '../toasts/toasts';
 import {LoadingSpinner} from '../loading-spinner/loading-spinner';
 import {ToDoStatusSelector} from '../to-do-status-selector/to-do-status-selector';
 import {ToDoCreateItem} from '../to-do-create-item/to-do-create-item';
+import {catchError, EMPTY, tap} from 'rxjs';
 
 @Component({
   selector: 'app-to-do-list',
@@ -52,13 +53,18 @@ export class ToDoList implements OnInit {
   private loadTasks(): void {
     this.isLoading.set(true)
     setTimeout(() => {
-      this.storageService.getTasks().subscribe({
-        next: (tasks: Task[]) => {
-          this.tasks.set(tasks)
-          this.toastService.showSuccess(`${this.tasks().length} tasks successfully loaded`)
-        },
-        error: (error: Error) => this.toastService.showError(`Tasks can't be loaded. Probably should run app with 'npm start' instead of 'ng serve'.\n${error.message}`),
-      })
+      this.storageService.getTasks()
+        .pipe(
+          tap((tasks: Task[]) => {
+            this.tasks.set(tasks)
+            this.toastService.showSuccess(`${this.tasks().length} tasks successfully loaded`)
+          }),
+          catchError((error: Error) => {
+            this.toastService.showError(`Tasks can't be loaded. Probably should run app with 'npm start' instead of 'ng serve'.\n${error.message}`)
+            return EMPTY
+          }),
+        )
+        .subscribe()
       this.isLoading.set(false)
     }, 500)
   }
@@ -72,23 +78,33 @@ export class ToDoList implements OnInit {
   }
 
   protected onDeleteTask(taskId: number, taskText: string): void {
-    this.storageService.deleteTask(taskId).subscribe({
-      next: () => {
-        this.tasks.update((tasks) => tasks.filter(t => t.id !== taskId))
-        this.toastService.showWarning(`Task '${taskText}' is deleted!`)
-      },
-      error: (error: Error) => this.toastService.showError(error.message),
-    })
+    this.storageService.deleteTask(taskId)
+      .pipe(
+        tap(() => {
+          this.tasks.update((tasks) => tasks.filter(t => t.id !== taskId))
+          this.toastService.showWarning(`Task '${taskText}' is deleted!`)
+        }),
+        catchError((error: Error) => {
+          this.toastService.showError(error.message)
+          return EMPTY
+        }),
+      )
+      .subscribe()
   }
 
   protected onAddTask(newTaskData: TaskData): void {
-    this.storageService.addTask(newTaskData).subscribe({
-      next: (task: Task) => {
-        this.toastService.showSuccess(`Task '${task.text}' is successfully added`)
-        this.tasks.update((taskList) => [...taskList, task])
-        this.selectedTaskId.set(this.DEFAULT_TASK_ID)
-      },
-      error: (error: Error) => this.toastService.showError(error.message),
-    })
+    this.storageService.addTask(newTaskData)
+      .pipe(
+        tap((task: Task) => {
+          this.toastService.showSuccess(`Task '${task.text}' is successfully added`)
+          this.tasks.update((taskList) => [...taskList, task])
+          this.selectedTaskId.set(this.DEFAULT_TASK_ID)
+        }),
+        catchError((error: Error) => {
+          this.toastService.showError(error.message)
+          return EMPTY
+        }),
+      )
+      .subscribe()
   }
 }
